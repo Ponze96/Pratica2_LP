0a78f30ea4fd454d5cc92696db9fa102ec0fdb1a
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-03-21 06:16:23 -0300

    Add files via upload

---
+++ Pratica2_LP/src/Questao2/MatrizDiagonal.java
@@ -0,0 +1,31 @@
+package Questao2;
+import java.util.Scanner;
+public class MatrizDiagonal {
+	 public static void main (String[] args){
+
+	        Scanner x[A][B] = new Scanner (System.in);
+
+	        int A[0],B[1],i, posicao = 50;
+	        
+	        int A[0],B[1], numeros = new int A[50],B[50];
+	        System.out.println("Digite 50 números inteiros seguidos:");
+
+	        while(posicao <= 49){
+	        numeros A[0],B[1] = x.nextInt();
+	        posicao++;
+	        }
+
+	        for(int[][] i = 1; i < numeros.length; i+=2){
+	            System.out.println("indice: " + i + " = " + numeros[i][i]);
+	        }             
+	    }    
+	 }
+	
+	
+	
+			
+	
+	
+	
+	
+
