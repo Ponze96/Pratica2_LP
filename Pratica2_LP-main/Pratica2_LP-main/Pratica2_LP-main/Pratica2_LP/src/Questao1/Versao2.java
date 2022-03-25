9b7fbe9bbc452de5556b92800578706b65824890
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-03-24 23:06:45 -0300

    Add files via upload

---
+++ Pratica2_LP-main/Pratica2_LP-main/Pratica2_LP-main/Pratica2_LP/src/Questao1/Versao2.java
@@ -0,0 +1,24 @@
+package Questao1;
+import java.util.Scanner;
+public class Versao2 {
+
+	public static void main1(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int op;
+		
+		System.out.println("Selecione a opção de pagamento ");
+        System.out.println("1 - Á Vista");
+        System.out.println("2 - 2 Vezes");
+        System.out.println("3 - 3 Vezes");
+        
+      
+	          	
+        		
+		
+		
+	}
+
+}
+	            
+
+
---
+++ Pratica2_LP-main/Pratica2_LP-main/Pratica2_LP-main/Pratica2_LP/src/Questao4/Operadora.java
@@ -0,0 +1,18 @@
+package Questao4;
+
+import java.util.Scanner;
+
+public class Operadora {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		String Nome, CPF, dataVoo, horario;
+		int Random;
+		int numeroVoo;
+		int opcao;
+		int escolha = 0;
+		
+	}
+
+}
